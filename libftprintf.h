f79d09bee776e7bdb6408c9ef9c9250509e55e8e
Luna Del Valle Blazquez <ldel-val@c1r11s3.42madrid.com>
2024-10-28 20:53:20 +0100

    updated library

--- libftprintf.h
+++ libftprintf.h
@@ -6,11 +6,9 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/10/26 17:01:10 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/10/28 11:27:07 by ldel-val          ``                     */
+/*   Updated: 2024/10/28 20:04:31 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
-
-#define	STDOUT 1
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
