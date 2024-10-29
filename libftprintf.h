8a991d08429507bff93b67444c5e72951d4b3fa6
luna <luna7111@tuta.io>
2024-10-29 01:23:15 +0100

    included libft

--- libftprintf.h
+++ libftprintf.h
@@ -6,18 +6,23 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/10/26 17:01:10 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/10/28 20:04:31 by ldel-val          ``                     */
+/*   Updated: 2024/10/29 00:13:06 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
+//variadic arguments
 # include <stdarg.h>
 //write
 # include <unistd.h>
 //printf
 # include <stdio.h>
-int	ft_printf(char const *format, ...);
+//libft
 # include "libft/libft.h"
+
+int	ft_print_specifier(char const *format, va_list arguments);
+int	ft_printf(char const *format, ...);
+
 #endif
