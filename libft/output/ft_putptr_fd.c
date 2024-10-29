f0ab9bb2d06b8873a39944587809d0d5f2bc27ac
Luna Del Valle Blazquez <ldel-val@c2r6s2.42madrid.com>
2024-10-29 15:31:11 +0100

    added null protection

--- libft/output/ft_putptr_fd.c
+++ libft/output/ft_putptr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.c     |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/10/28 16:10:57 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/10/28 18:02:56 by ldel-val          ``                     */
+/*   Updated: 2024/10/29 15:22:54 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_putptr_fd(void *ptr, int fd)
 {
 	int	printed_bytes;
 
+	if (!ptr)
+		return (write(fd, "(nil)", 5));
 	printed_bytes = write(fd, "0x", 2);
 	printed_bytes += ft_puthex_fd((size_t)ptr, fd);
 	return (printed_bytes);
--- libft/output/ft_putstr_fd.c
+++ libft/output/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/10/26 15:43:39 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/10/28 11:50:35 by ldel-val          ``                     */
+/*   Updated: 2024/10/29 15:22:17 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_putstr_fd(char *s, int fd)
 	int	printed_bytes;
 
 	printed_bytes = 0;
+	if (!s)
+		return (write(fd, "(null)", 6));
 	while (*s)
 	{
 		printed_bytes += write(fd, s, 1);
